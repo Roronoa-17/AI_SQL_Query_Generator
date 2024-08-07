613b3fdadabfa49f817e6f7b918bb55248f5d0c6
Priyesh H. Gawali <thepriyesh17@gmail.com>
2024-08-07 22:49:50 +0530

    main app file

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st 
+import google.generativeai as genai
+
+
+genai.configure(api_key="AIzaSyDc3u4wMi-IxuGaxYy27rNjntsqYpYBEj0")
+
+model = genai.GenerativeModel('gemini-pro')
+
+
+
+st.set_page_config(page_title="AI SQL Query Generator")
+st.markdown(
+    """ 
+    <div style="text-align:center;">
+    <h1> SQL Query Generator using LLM </h1>
+    <h3>This tool can generate SQL queries for given prompt</h3>
+    <h3>With Explanation of Queries </h3>
+    </div>
+    """,
+    unsafe_allow_html=True, 
+)
+
+text_input = st.text_area("Enter you Query here:")
+
+submit = st.button("Generate SQL Query")
+
+if submit:
+    with st.spinner("Generating SQL Query..."):
+        template = """ 
+        Generate a SQL query for 
+        '''
+        {text_input}
+        '''
+        Also explain the query and concepts used in generated query.
+        """
+        formatted_template = template.format(text_input=text_input)
+        
+        # st.write(formatted_template)
+        response = model.generate_content(formatted_template)
+        sql_query = response.text
+        st.write("Generated query with explaination: ")
+        st.write(sql_query)   
+    
